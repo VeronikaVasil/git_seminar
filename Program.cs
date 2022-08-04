0ff9d6af54c62a0188f79d1ae14f7c3bd2e033c5
Veronika <valeria14081981@yandex.com>
2022-08-05 01:07:32 +0300

    Числа целые подряд от - до +

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число N:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int negativeNumber = number * (-1);
+
+while (negativeNumber <= number) //пока несправедливо условие
+{
+    Console.WriteLine(negativeNumber);
+    negativeNumber++;
+}
+
