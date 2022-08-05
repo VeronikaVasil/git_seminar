dbcfe99658b971071bf37f6ab560780e57d53c5e
Veronika <valeria14081981@yandex.com>
2022-08-06 02:02:53 +0300

    Первый семинар

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Write("Введите число N:");
+﻿//Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.//
+Console.Write("Введите число N:");
 int number = Convert.ToInt32(Console.ReadLine());
 
 int negativeNumber = number * (-1);
